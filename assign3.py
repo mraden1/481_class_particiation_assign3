71dcbfc17e00d2432afee9a397e1027a59525323
mraden <143109899+mraden1@users.noreply.github.com>
2023-09-29 16:34:01 -0700

    trying to follow along with professor in class

---
+++ assign3.py
@@ -0,0 +1,119 @@
+import pprint
+import ipywidgets as widgets
+
+import sys
+import matplotlib.pyplot as plt
+import networkx as nx
+
+
+class WeightedGraph:
+  def __init__(self):
+    self.all_nodes = {}
+
+  def addNode(self, name, adj_nodes_with_cost):   #node has form:
+    node_content_dict = {}
+    node_content_dict.update(adj_nodes_with_cost) #{"name":{"adj_node1":cost1, "adj_node2:cost2",......}}
+    self.all_nodes[name] = node_content_dict
+
+  def plotWeightedGraph(self):
+    G = nx.Graph()
+    for curr_node in self.all_nodes:
+      curr_node_str = curr_node
+      for curr_adj_node in self.all_nodes[curr_node]:
+        if curr_adj_node == 'h_val':
+          continue
+        adj_node_str = curr_adj_node
+        edge_cost = self.all_nodes[curr_node][curr_adj_node]
+        G.add_edge(curr_node_str, adj_node_str, weight=edge_cost)
+
+    pos = nx.spring_layout(G, seed=180)
+    # nodes
+    nx.draw_networkx_nodes(G, pos, node_size=1000)
+    # edges
+    nx.draw_networkx_edges(G, pos)
+    # node labels
+    nx.draw_networkx_labels(G, pos, font_size=15)
+    # edge weight labels
+    edge_labels = nx.get_edge_attributes(G, "weight")
+    nx.draw_networkx_edge_labels(G, pos, edge_labels=edge_labels)
+    #draw
+    plt.axis("off")
+    plt.tight_layout()
+    plt.show()#implement the rest of the class
+
+class SampleUCS:
+  def __init__(self):
+    self.SampleGraph = WeightedGraph()
+    self.p_queue = []
+    self.visited = []
+  # you may add (a lot) more helper functions here
+
+  def getEdgeCost(self,start_node,end_node):
+    return self.SampleGraph[start_node][end_node]
+
+    def expandNode(self):
+        for curr_adj_node in self.SampleGraph[exp_node]:
+            
+            new_cost = self.getEdgeCost(exp_node, curr_adj_nodes) + self.visited[exp_node]["path cost"]
+            print("new cost is: " + new_cost)
+            new_path = self.visited[exp_node] = ", " + curr_adj_node
+            print("new path is : " + new_path)
+            data_to_add = (curr_adj_node, new_path, new_cost)
+            print("new data to add is: " + data_to_add)
+
+            add_switch = True
+            for curr_data in self.p_queue:
+                if exp_node in curr_data: 
+                    add_switch = False
+            for curr_node in self.visited:
+                if exp_node == curr_node:
+                    add_switch = False
+
+
+            if add_switch == True:
+                self.p_queue.append(data_to_Add)
+    
+    self.p_queue.sort()
+    return
+
+  def sampleUCS(self, start_node, target_node):
+    #implement your algorithm here
+    self.visited.append(start_node) = ("path":start_node, "path_cost":0)
+    self.expandNode(start_node)
+
+    return
+
+
+'''
+Initialize frontier with initial state
+Initialize explored to empty
+Loop do
+ 1 IF the frontier is empty RETURN FAILURE
+ 2 Choose lowest cost node from frontier and remove it
+ 3 IF lowest cost node is goal RETURN SUCCESS
+ 4 Add node to explored
+ 5 FOR every child node of node:
+   IF child not already on frontier or explored:
+    insert child node to frontier
+   ELSE IF child is in frontier with higher path cost:
+    replace existing frontier node with child node
+'''
+
+G = SampleUCS()
+G.SampleGraph.addNode("A", {"B":2,"E":3})
+G.SampleGraph.addNode("B", {"A":2,"C":3,"F":3})
+G.SampleGraph.addNode("C", {"B":2,"D":3,"G":3})
+G.SampleGraph.addNode("D", {"C":2,"H":3})
+
+G.SampleGraph.addNode("E", {"A":2,"I":3})
+G.SampleGraph.addNode("F", {"B":2,"J":3,"E":3,"G":3})
+G.SampleGraph.addNode("G", 3, {"C":2,"K":3,"F":3,"H":3})
+G.SampleGraph.addNode("H", 2, {"D":2,"L":3,"G":3})
+
+G.SampleGraph.addNode("I", 6, {"E":2,"J":3})
+G.SampleGraph.addNode("J", 4, {"F":2,"I":3,"K":3})
+G.SampleGraph.addNode("K", 2, {"G":2,"J":3,"L":3})
+G.SampleGraph.addNode("L", 0, {"H":2,"K":3})
+
+G.SampleGraph.plotWeightedGraph()
+G.visited["A"]=
